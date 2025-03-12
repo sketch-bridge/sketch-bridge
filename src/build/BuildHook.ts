18afaa5fd35352695427e816947a9186cef85029
Yoichiro Tanaka <yoichiro6642@gmail.com>
2025-03-12 17:41:12 +0900

    Change the URL for development.

--- src/build/BuildHook.ts
+++ src/build/BuildHook.ts
@@ -23,17 +23,20 @@ export const useBuilder = () => {
       setOutput('');
       showNotification('Building your sketch...', 'info');
       const idToken = await firebaseAuth.user.getIdToken();
-      // const response = await fetch(`http://localhost:8080/build`, {
-      const response = await fetch(`/build`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${idToken}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-          projectId: currentProject.id,
-        }).toString(),
-      });
+      const response = await fetch(
+        `https://arduino-compile-server-163071802807.asia-northeast1.run.app/build`,
+        {
+          // const response = await fetch(`/build`, {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${idToken}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            projectId: currentProject.id,
+          }).toString(),
+        }
+      );
       if (!response.ok) {
         console.error(response.statusText);
         setBuildResult('failure');
